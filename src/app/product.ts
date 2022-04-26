6128f801c8ef56a0c7cddb820d0d06c3bae41164
Siarhei Zubarau <barclays1313@gmail.com>
2022-04-26 18:03:29 +0300

    fix(master): refactor component

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,12 +1,7 @@
 export interface Product {
-    id?:string;
-    code?:string;
     name?:string;
-    description?:string;
-    price?:number;
-    quantity?:number;
-    inventoryStatus?:string;
     category?:string;
-    image?:string;
-    rating?:number;
+    version?:string;
+    url?:string;
+    description?:string;
 }
